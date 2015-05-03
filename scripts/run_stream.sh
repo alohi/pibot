6ea9e5f22d0496b1637e723ad1efb2c703960e3a
stonedmind <stonrmind@gmail.com>
2015-05-03 15:58:52 +0530

    small changes

--- scripts/run_stream.sh
+++ scripts/run_stream.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-# run_cam.sh
+# run_stream.sh
 # This will ad to crontab
-# @reboot sh /home/pi/pibot/scripts/run_cam.sh > /tmp/pibot_cam_log 2>&1
-cd /
-cd /home/pi/pibot/src
-python cam.py
+# @reboot sh /home/pi/pibot/scripts/run_stream.sh > /tmp/pibot_stream_log 2>&1
+cd /home/pi/mjpg-streamer/mjpg-streamer-experimental
+LD_LIBRARY_PATH=/usr/local/lib ./mjpg_streamer -i "input_file.so -f 15 -r 1280x720 -f /tmp -n pic.jpg" -o "output_http.so -w /usr/local/www"
--- src/cam.py
+++ src/cam.py
@@ -5,6 +5,6 @@ import sys
 os.mkdir("/tmp/stream")
 while True:
 	print 'capturing frame'
-	os.system("fswebcam -d /dev/video0 -r 1280x720 /tmp/stream/pic.jpg")
+	os.system("fswebcam -d /dev/video0 -r 1280x720 /tmp/pic.jpg")
 	print "captured"
 	time.sleep(2)
